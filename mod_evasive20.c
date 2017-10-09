3dd7c72166fbd6c3ae6701354875a7bb5759e990
Joris Vandermeersch <joris.vdmr@gmail.com>
2017-10-09 15:03:59 +0200

    fixed Apache module name

--- mod_evasive20.c
+++ mod_evasive20.c
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "http_log.h"
 #include "http_request.h"
 
-module AP_MODULE_DECLARE_DATA evasive20_module;
+module AP_MODULE_DECLARE_DATA evasive_module;
 
 /* BEGIN DoS Evasive Maneuvers Definitions */
 
@@ -703,7 +703,7 @@ static void register_hooks(apr_pool_t *p) {
   apr_pool_cleanup_register(p, NULL, apr_pool_cleanup_null, destroy_hit_list);
 };
 
-module AP_MODULE_DECLARE_DATA evasive20_module =
+module AP_MODULE_DECLARE_DATA evasive_module =
 {
     STANDARD20_MODULE_STUFF,
     NULL,
