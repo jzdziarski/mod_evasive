319cec3773c847fbf03f647aaefdedb4dc72f374
Joris Vandermeersch <joris.vdmr@gmail.com>
2017-10-10 11:03:57 +0200

    made mod_evasive configurable per-vhost instead of globally

--- test.pl
+++ test.pl
@@ -1,18 +1,28 @@
 #!/usr/bin/perl
 
 # test.pl: small script to test mod_evasive's effectiveness
+# - requires virtualhosts a.site and b.site to be setup, each with their own mod_evasive config
+# - requires a.site and b.site to be added to hosts file (probably)
 
 use IO::Socket;
 use strict;
 
-for(0..100) {
+sub request {
+	my($address,$i) = @_;
   my($response);
   my($SOCKET) = new IO::Socket::INET( Proto   => "tcp",
                                       PeerAddr=> "127.0.0.1:80");
   if (! defined $SOCKET) { die $!; }
-  print $SOCKET "GET /?$_ HTTP/1.0\n\n";
+  print $SOCKET "GET /?$i HTTP/1.1\r\n";
+  print $SOCKET "Host: $address\r\n\r\n";
   $response = <$SOCKET>;
-  print $response;
+  print $address . ": " . $response;
   close($SOCKET);
 }
 
+for(0..100) {
+	request "a.site", $_;
+}
+for(0..50) {
+	request "b.site", $_;
+}
