da79f2c3aa55c0ab1c18fdf0b7656a65e09c657a
Attila <1230402+borzaka@users.noreply.github.com>
2019-09-10 23:05:09 +0200

    Update test.pl to working order
    HTTP/1.1 400 Bad Request without this modificaiton
    Reference: https://centosfaq.org/centos/apache-mod_evasive-problem-with-testpl/
    Tested on: Debian 9, Debian 10

--- test.pl
+++ test.pl
@@ -10,7 +10,7 @@ for(0..100) {
   my($SOCKET) = new IO::Socket::INET( Proto   => "tcp",
                                       PeerAddr=> "127.0.0.1:80");
   if (! defined $SOCKET) { die $!; }
-  print $SOCKET "GET /?$_ HTTP/1.0\n\n";
+  print $SOCKET "GET /?$_ HTTP/1.0\r\nHost: 127.0.0.1\r\n\r\n";
   $response = <$SOCKET>;
   print $response;
   close($SOCKET);
