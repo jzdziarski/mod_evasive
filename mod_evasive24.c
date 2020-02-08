022860f29c6964059e4e478e7e30c852564089b8
Joris Vandermeersch <joris@vdmr.be>
2020-02-08 12:17:12 +0100

    Add DOSWhitelistUri config option

--- mod_evasive24.c
+++ mod_evasive24.c
@@ -33,6 +33,9 @@
 #include <syslog.h>
 #include <errno.h>
 
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+
 #include "httpd.h"
 #include "http_core.h"
 #include "http_config.h"
@@ -85,6 +88,7 @@ struct ntt_c {
 struct ntt *ntt_create(unsigned long size);
 int ntt_destroy(struct ntt *ntt);
 struct ntt_node *ntt_find(struct ntt *ntt, const char *key);
+struct ntt_node *ntt_re_find(struct ntt *ntt, const char *key);
 struct ntt_node *ntt_insert(struct ntt *ntt, const char *key, time_t timestamp);
 int ntt_delete(struct ntt *ntt, const char *key);
 long ntt_hashcode(struct ntt *ntt, const char *key);
@@ -96,11 +100,17 @@ struct ntt_node *c_ntt_next(struct ntt *ntt, struct ntt_c *c);
 
 /* BEGIN DoS Evasive Maneuvers Globals */
 
+struct pcre_node {
+    pcre2_code *re;
+    struct pcre_node *next;
+};
+
 typedef struct {
     int enabled;
     char *context;
     struct ntt *hit_list;   // Our dynamic hash table
     unsigned long hash_table_size;
+    struct pcre_node *uri_whitelist;
     int page_count;
     int page_interval;
     int site_count;
@@ -115,6 +125,9 @@ typedef struct {
 static const char *whitelist(cmd_parms *cmd, void *dconfig, const char *ip);
 int is_whitelisted(const char *ip, evasive_config *cfg);
 
+static const char *whitelist_uri(cmd_parms *cmd, void *dconfig, const char *uri_re);
+int is_uri_whitelisted(const char *uri, evasive_config *cfg);
+
 /* END DoS Evasive Maneuvers Globals */
 
 static void * create_dir_conf(apr_pool_t *p, char *context)
@@ -127,6 +140,7 @@ static void * create_dir_conf(apr_pool_t *p, char *context)
         cfg->context = strdup(context);
         cfg->hash_table_size = DEFAULT_HASH_TBL_SIZE;
         cfg->hit_list = ntt_create(cfg->hash_table_size);
+        cfg->uri_whitelist = NULL;
         cfg->page_count = DEFAULT_PAGE_COUNT;
         cfg->page_interval = DEFAULT_PAGE_INTERVAL;
         cfg->site_count = DEFAULT_SITE_COUNT;
@@ -150,6 +164,45 @@ static const char *whitelist(cmd_parms *cmd, void *dconfig, const char *ip)
     return NULL;
 }
 
+static const char *whitelist_uri(cmd_parms *cmd, void *dconfig, const char *uri_re)
+{
+    evasive_config *cfg = (evasive_config *) dconfig;
+    struct pcre_node *node;
+
+    node = (struct pcre_node *) malloc(sizeof(struct pcre_node));
+    if (node == NULL) {
+        return NULL;
+    }
+
+    int errornumber;
+    PCRE2_SIZE erroroffset;
+
+    PCRE2_SPTR pattern;
+    pattern = (PCRE2_SPTR) uri_re;
+
+    node->re = pcre2_compile(
+            pattern,               /* the pattern */
+            PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
+            0,                     /* default options */
+            &errornumber,          /* for error number */
+            &erroroffset,          /* for error offset */
+            NULL);                 /* use default compile context */
+
+    /* Compilation failed: print the error message and exit. */
+
+    if (node->re == NULL)
+    {
+        PCRE2_UCHAR buffer[256];
+        pcre2_get_error_message(errornumber, buffer, sizeof(buffer));
+        printf("PCRE2 compilation failed at offset %d: %s\n", (int)erroroffset,
+                buffer);
+        return NULL;
+    }
+
+    node->next = cfg->uri_whitelist;
+    cfg->uri_whitelist = node;
+    return NULL;
+}
 
 static int access_checker(request_rec *r)
 {
@@ -180,8 +233,13 @@ static int access_checker(request_rec *r)
             /* Not on hold, check hit stats */
         } else {
 
+            /* Check whitelisted uris */
+            if (is_uri_whitelisted(r->uri, cfg))
+                return OK;
+
             /* Has URI been hit too much? */
             snprintf(hash_key, 2048, "%s_%s", r->useragent_ip, r->uri);
+
             n = ntt_find(cfg->hit_list, hash_key);
             if (n != NULL) {
 
@@ -320,6 +378,46 @@ int is_whitelisted(const char *ip, evasive_config *cfg) {
     return 0;
 }
 
+int is_uri_whitelisted(const char *path, evasive_config *cfg) {
+
+    int rc;
+    pcre2_match_data *match_data;
+
+    PCRE2_SPTR subject;
+    size_t subject_length;
+
+    subject = (PCRE2_SPTR) path;
+    subject_length = strlen((char *)subject);
+
+    struct pcre_node *node;
+    node = cfg->uri_whitelist;
+
+    while (node != NULL) {
+        match_data = pcre2_match_data_create_from_pattern(node->re, NULL);
+
+        rc = pcre2_match(
+                node->re,             /* the compiled pattern */
+                subject,              /* the subject string */
+                subject_length,       /* the length of the subject */
+                0,                    /* start at offset 0 in the subject */
+                0,                    /* default options */
+                match_data,           /* block for storing the result */
+                NULL);                /* use default match context */
+
+        pcre2_match_data_free(match_data);   /* Release memory used for the match */
+
+        if (rc >= 0) {
+            // match
+            return 1;
+        }
+
+        node = node->next;
+    }
+
+    // no match
+    return 0;
+}
+
 static apr_status_t destroy_config(void *dconfig) {
     evasive_config *cfg = (evasive_config *) dconfig;
     ntt_destroy(cfg->hit_list);
@@ -446,7 +544,7 @@ struct ntt_node *ntt_insert(struct ntt *ntt, const char *key, time_t timestamp) 
     /* Create a new node */
     new_node = ntt_node_create(key);
     new_node->timestamp = timestamp;
-    new_node->timestamp = 0;
+    new_node->count = 0;
 
     ntt->items++;
 
@@ -740,6 +838,9 @@ static const command_rec access_cmds[] =
 
     AP_INIT_ITERATE("DOSWhitelist", whitelist, NULL, RSRC_CONF,
             "IP-addresses wildcards to whitelist"),
+
+    AP_INIT_ITERATE("DOSWhitelistUri", whitelist_uri, NULL, RSRC_CONF,
+            "Files/paths regexes to whitelist"),
 
     AP_INIT_ITERATE("DOSHTTPStatus", get_http_reply, NULL, RSRC_CONF,
             "HTTP reply code"),
